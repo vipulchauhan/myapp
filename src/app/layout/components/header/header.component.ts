ae782cdd31594ee25fb3a153ba895c41f49f4164
vipul <08bce016@gmail.com>
2018-09-01 20:28:38 +0530

    personalized

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -9,13 +9,27 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
     pushRightClass: string = 'push-right';
+    userName = 'Vipul Chauhan';
+    appName = 'My App';
 
     constructor(private translate: TranslateService, public router: Router) {
-
-        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
+        this.translate.addLangs([
+            'en',
+            'fr',
+            'ur',
+            'es',
+            'it',
+            'fa',
+            'de',
+            'zh-CHS'
+        ]);
         this.translate.setDefaultLang('en');
         const browserLang = this.translate.getBrowserLang();
-        this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
+        this.translate.use(
+            browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/)
+                ? browserLang
+                : 'en'
+        );
 
         this.router.events.subscribe(val => {
             if (
